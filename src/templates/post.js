929c139ff87b3452d2644948709cc665f79d0253
Stackbit Code Editor <projects@stackbit.com>
2021-08-14 07:27:20 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -58,7 +58,7 @@ export default class Post extends React.Component {
             	<div className="container container--medium">
             		<div className="post__body text-block">
             			{htmlToReact(_.get(this.props, 'pageContext.html', null))}
-                        
+                        <SectionActions {...this.props} actions={_.get(section, 'actions', null)} />
             		</div>
             		{_.get(this.props, 'pageContext.frontmatter.tags', null) && (
             		<footer className="post__footer mt-4 mt-md-5">
